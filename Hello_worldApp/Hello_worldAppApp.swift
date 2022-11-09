ee10477ddb78f8bb4482884dc93759b7cc6a7c34
AndrewDubey <gunter015@gmail.com>
2022-11-09 10:14:19 +0500

    Initial Commit

---
+++ Hello_worldApp/Hello_worldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_worldAppApp.swift
+//  Hello_worldApp
+//
+//  Created by Andrew on 05.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Hello_worldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
